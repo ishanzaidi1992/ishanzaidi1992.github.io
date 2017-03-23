fcbce5ca27c13bb281b03c13741226177134e142
ishanzaidi1992 <ishanzaidi1992@gmail.com>
2017-03-23 23:18:30 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
   $('.clash-card__image img').hide();
   $('.slick-active').find('.clash-card img').fadeIn(200);
   $('.clash-card__image .last').hide();
-  $('.slick-active').find('.clash-card .last').fadeIn(200);
+  $('.slick-active').find('.clash-card__image .last').fadeIn(200);
   
   // On before slide change
   slideContainer.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
