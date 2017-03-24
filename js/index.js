a4b0fa0c83890879f396d2e183ec6db9263befad
ishanzaidi1992 <ishanzaidi1992@gmail.com>
2017-03-24 14:55:56 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,16 +4,16 @@
   slideContainer.slick();
   
   $('.clash-card__image img').hide();
-  $('.slick-active').find('.clash-card img').fadeIn(200);
+  $('.slick-active').find('.clash-card img');
   
   // On before slide change
   slideContainer.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-    $('.slick-active').find('.clash-card img').fadeOut(1000);
+    $('.slick-active').find('.clash-card img');
   });
   
   // On after slide change
   slideContainer.on('afterChange', function(event, slick, currentSlide) {
-    $('.slick-active').find('.clash-card img').fadeIn(200);
+    $('.slick-active').find('.clash-card img');
   });
 
   
